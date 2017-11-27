0cbfc1e6e91d3703786f3ab144f51e332db0fa33
RonaldLumu <ronald12feb@hotmail.co.uk>
2017-11-27 19:27:21 +0000

    Orders DTO

---
+++ CmsShoppingCart/Models/Data/OrderDetailsDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CmsShoppingCart.Models.Data
+{
+    [Table("tblOrderDetails")]
+    public class OrderDetailsDTO
+    {
+        [Key]
+        public int Id { get; set; }     //primary key
+        public int OrderId { get; set; } //foreign key
+        public int UserId { get; set; }  //foreign key
+        public int ProductId { get; set; } //foreign key
+        public int Quantity { get; set; }  //foreign key
+
+        [ForeignKey("OrderID")]
+        public virtual OrderDTO Orders { get; set; }
+        [ForeignKey("UserId")]
+        public virtual UserDTO Users { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product Products { get; set; }
+         
+
+
+    }
+}
